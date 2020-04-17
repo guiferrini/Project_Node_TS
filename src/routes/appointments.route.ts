e95cf806f995e6bcfdc02c691184fcc7e9dcd56b
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-04-17 12:01:09 -0300

    control - authenticate the date of the same appointment

--- src/routes/appointments.route.ts
+++ src/routes/appointments.route.ts
@@ -1,18 +1,37 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = []; // P salvar os agendamentos
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = []; // P salvar os agendamentos
 
 appointmentsRouter.post('/', (request, response) => {
   // só coloco '/', pq já foi definida a rota e exportada
   const { provider, date } = request.body; // nome do barber e data(dia e hr)
 
+  const parsedDate = startOfHour(parseISO(date)); // zera hora e transforma date em obj js
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
