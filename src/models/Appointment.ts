48aed6d06954ccb188a31be328ae0f6df4ea6dcc
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-04-23 14:59:42 -0300

    Add table appointments and model appointments

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,19 +1,25 @@
 // responsabilidade pelo formato dos dados
-import { uuid } from 'uuidv4';
+// import { uuid } from 'uuidv4'; qdo usava o constructor
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('time with time zone')
   date: Date;
 
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    // Omit(excluir a propriedade de dentro de um tipo): 1°tipo, 2° o q quero omitir, omito ID, pq gera sozinho
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
+  // COnstructor é p class, mas p entidade do typeorm n necessita, ele cria 'automática'
+  // constructor({ provider, date }: Omit<Appointment, 'id'>) {
+  // Omit(excluir a propriedade de dentro de um tipo): 1°tipo, 2° o q quero omitir, omito ID, pq gera sozinho
+  // this.id = uuid();
+  // this.provider = provider;
+  // this.date = date;
+  // }
 }
 
 export default Appointment;
