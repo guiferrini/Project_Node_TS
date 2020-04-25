e5d6d0c068625b5248d02855169eb195282859dc
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-04-25 15:45:58 -0300

    Create Users(route and services)

--- src/routes/appointments.route.ts
+++ src/routes/appointments.route.ts
@@ -17,7 +17,7 @@ appointmentsRouter.get('/', async (request, response) => {
 appointmentsRouter.post('/', async (request, response) => {
   // só coloco '/', pq já foi definida a rota e exportada
   try {
-    const { provider, date } = request.body; // nome do barber e data(dia e hr)
+    const { provider_id, date } = request.body; // nome do barber e data(dia e hr)
 
     // const parsedDate = startOfHour(parseISO(date)); // zera hora e transforma date em obj js
     const parsedDate = parseISO(date);
@@ -26,7 +26,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider,
+      provider_id,
     });
 
     return response.json(appointment);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@
 // A ROTA: Recebe requisição; Chama outro arquivo p tratar; devolve uma resposta
 import { Router } from 'express';
 import appointmentsRouter from './appointments.route';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter); // td rota '/apointments da pasta appointments usará esta rota
+routes.use('/users', usersRouter);
 
 export default routes;
