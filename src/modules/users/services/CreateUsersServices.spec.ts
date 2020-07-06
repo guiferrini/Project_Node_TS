5516eefd89e52cc1d0d8acb0a04e1efb2c4a96e9
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-07-06 19:49:46 -0300

    Add CreateUsersServices.spec: CreateUser - should be able to create a new user with the same email from another

--- src/modules/users/services/CreateUsersServices.spec.ts
+++ src/modules/users/services/CreateUsersServices.spec.ts
@@ -1,6 +1,6 @@
 // test() = it()
 // teste unitario, cria o fake repository - n utiliza o BD 'real'
-// import AppError from '@shared/errors/AppErrors';
+import AppError from '@shared/errors/AppErrors';
 import CreateUserServices from './CreateUsersService';
 import FakeUsersRepository from '../repositories/fake/FakeUsersRepository';
 
@@ -17,5 +17,25 @@ describe('CreateUser', () => {
     });
 
     expect(user).toHaveProperty('id');
+  });
+
+  it('should be able to create a new user with the same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    // Criou o service e passou o repository fake, salva as infos na memoria da aplicação
+    const createUser = new CreateUserServices(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'gui',
+      email: 'gmf@gmail.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'gui',
+        email: 'gmf@gmail.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
