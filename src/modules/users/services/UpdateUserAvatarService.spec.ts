78657e00c209d64830c22d6b3c281537511123fd
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-07-07 16:05:59 -0300

    Add UpdateUserAvatarService.spec: UpdateUsersAvatar - should not be able to update avatar with not existing user

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -1,6 +1,6 @@
 // test() = it()
 // teste unitario, cria o fake repository - n utiliza o BD 'real'
-// import AppError from '@shared/errors/AppErrors';
+import AppError from '@shared/errors/AppErrors';
 import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
 import UpdateUserAvatarService from './UpdateUserAvatarService';
 import FakeUsersRepository from '../repositories/fake/FakeUsersRepository';
@@ -28,5 +28,23 @@ describe('UpdateUserAvatar', () => {
     });
 
     expect(user.avatar).toBe('avatarTest.jpg');
+  });
+
+  it('should not be able to update avatar with not existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    // Criou o service e passou o repository fake, salva as infos na memoria da aplicação
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    expect(
+      updateUserAvatar.execute({
+        user_id: 'non-exist-user',
+        avatarFilename: 'avatarTest.jpg',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
