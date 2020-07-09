a58ae3064c3bd056f88c11088030550bd63145eb
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-07-09 15:09:35 -0300

    Add EtherealMailProvider and Nodemailer

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementantions/DiskStorageProvider';
 
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementantions/EtherealMailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider, // hj estatico, no futuro um caminho p desenvolvimento outro p produção
+);
+
+container.registerSingleton<IMailProvider>(
+  'MailProvider',
+  EtherealMailProvider, // hj estatico, no futuro um caminho p desenvolvimento outro p produção
 );
