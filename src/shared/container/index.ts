bc420e9582e374a242e6ebb249e9a30b51fa105b
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-07-09 12:29:36 -0300

    Add dependency injection to UserTokensRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,8 +9,8 @@ import AppointmetsRepositoy from '@modules/appointments/infra/typeorm/repositori
 import IUsersRepository from '@modules/users/repositories/IUsersRepositories';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepositories';
 
-// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UsersRepositories';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IAppointmetsRepository>( // a Tipagem garante q 2° parametro esteja no formato correto
   'AppointmetsRepositoy', // id/nome (eu escolho)
@@ -20,4 +20,9 @@ container.registerSingleton<IAppointmetsRepository>( // a Tipagem garante q 2° 
 container.registerSingleton<IUsersRepository>( // a Tipagem garante q 2° parametro esteja no formato correto
   'UsersRepository', // id/nome (eu escolho)
   UsersRepository, // repositorio q vai usar na injeção
+);
+
+container.registerSingleton<IUserTokensRepository>( // a Tipagem garante q 2° parametro esteja no formato correto
+  'UserTokensRepository', // id/nome (eu escolho)
+  UserTokensRepository, // repositorio q vai usar na injeção
 );
