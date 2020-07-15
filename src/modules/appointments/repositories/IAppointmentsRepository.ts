1de68db35fed4187e5704b7cf2e027300742c0a0
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-07-15 20:33:03 -0300

    Add IAppointmentsRepository: findAllMonthFromProvider

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -3,8 +3,10 @@
 // Quais metodos existem no nosso Repository
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dto/ICreateAppointmenDTO';
+import IFindAllMonthFromProvider from '../dto/IFindAllMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllMonthFromProvider(date: IFindAllMonthFromProvider): Promise<Appointment[]>;
 }
