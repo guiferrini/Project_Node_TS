a5e807bc890b274fb2d938c6d583b605003239c3
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-07-17 19:51:48 -0300

    Add AppointmentsRepository: findAllDayFromProvider

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -6,7 +6,7 @@ import { getRepository, Repository, Raw } from 'typeorm';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dto/ICreateAppointmenDTO';
 import IFindAllMonthFromProviderDTO from '@modules/appointments/dto/IFindAllMonthFromProviderDTO';
-
+import IFindAllDayFromProviderDTO from '@modules/appointments/dto/IFindAllDayFromProviderDTO';
 
 import Appointment from '../entities/Appointment';
 
@@ -37,6 +37,27 @@ class AppointmentsRepository implements IAppointmentsRepository {
           date: Raw(
             dateFilename =>
               `to_char(${dateFilename}, 'MM-YYYY') = '${parseMonth}-${year}'`,
+          ),
+        },
+      });
+
+    return appointments;
+  }
+
+  public async findAllDayFromProvider({
+    provider_id,
+    month,
+    day,
+    year }: IFindAllDayFromProviderDTO): Promise<Appointment[]> {
+      const parseMonth = String(month).padStart(2, '0'); //se começa com 1, transforma em 01
+      const parseDay = String(day).padStart(2, '0'); //se começa com 1, transforma em 01
+
+      const appointments = await this.ormRepository.find({
+        where: {
+          provider_id,
+          date: Raw(
+            dateFilename =>
+              `to_char(${dateFilename}, 'DD-MM-YYYY') = '${parseDay}-${parseMonth}-${year}'`,
           ),
         },
       });
