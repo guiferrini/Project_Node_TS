5fe346e88811ecbaceb393d042a9db3339ebd08e
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-07-17 19:53:29 -0300

    Add ListProviderDayAvaibilityService: should be able to list the day avaibility from providers

---
+++ src/modules/appointments/services/ListProviderDayAvaibilityService.ts
@@ -0,0 +1,64 @@
+import 'reflect-metadata';
+import { inject, injectable, container } from 'tsyringe';
+import { getHours } from 'date-fns';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  day: number;
+  year: number;
+}
+
+// objeto dentro de um Array -> [ {...} {...} {...} ]
+type IResponse = Array<{
+  hour: number;
+  availabla: boolean;
+}>;
+
+
+@injectable()
+class ListProviderDayAvaibilityServices {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+    day
+  }: IRequest): Promise<IResponse> {
+    const appointments = await  this.appointmentsRepository.findAllDayFromProvider({
+      provider_id,
+      month,
+      year,
+      day
+    });
+
+    const hourStart = 8;
+
+    const eachHourArray = Array.from(
+      { length: 10 },
+      (_, index) => index + hourStart,
+    );
+
+    const availability = eachHourArray.map(hour => {
+      const hasAppointmentInHour = appointments.find(
+        appointment => getHours(appointment.date) === hour,
+      );
+
+      return {
+        hour,
+        availabla: !hasAppointmentInHour // o contrario do has..., devolve um boolean
+      }
+    })
+
+return availability;
+};
+};
+export default ListProviderDayAvaibilityServices;
