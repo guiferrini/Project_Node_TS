0972baa84fe79f6e6bc6debd0158c937e0b5fb9c
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-07-18 11:05:54 -0300

    Update FakeAppointmenst

--- src/modules/appointments/repositories/fakes/fakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/fakeAppointmentsRepository.ts
@@ -56,11 +56,12 @@ class AppointmentsRepository implements IAppointmentsRepository {
 
   public async create({
     provider_id,
+    user_id,
     date,
   }: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = new Appointment();
 
-    Object.assign(appointment, { id: uuid(), date, provider_id });
+    Object.assign(appointment, { id: uuid(), date, provider_id, user_id });
     // mesmo resultado do object, uma unica linha
     // appointment.id = uuid();
     // appointment.provider_id = provider_id;
